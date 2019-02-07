3259be6f88e2ffe78fbc5058e66dcef9b4cb261a
Andres Rivera <andres.rivera.toro@gmail.com>
2019-02-07 04:19:24 -0500

    Add tests for Controls component

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,29 +1,42 @@
 import React from 'react';
 import { render } from 'react-testing-library';
 import 'react-testing-library/cleanup-after-each';
+import 'jest-dom/extend-expect';
 
 import Display from './Display';
 
-
 describe('<Display />', () => {
-  it('Unlocked and open', () => {
+  it('Displays unlocked and open', () => {
     const { getByText } = render(<Display closed={false} locked={false} /> );
-
     getByText(/unlocked/i);
     getByText(/open/i);
   });
 
-  it('Unlocked and closed', () => {
+  it('Displays unlocked and closed', () => {
     const { getByText } = render(<Display closed={true} locked={false} /> );
-
     getByText(/unlocked/i);
     getByText(/closed/i);
   });
 
-  it('Locked and closed', () => {
+  it('Displays locked and closed', () => {
     const { getByText } = render(<Display closed={true} locked={true} /> );
-
     getByText(/locked/i);
     getByText(/closed/i);
+  });
+
+  it('Uses green-led class when unlocked or open', () => {
+    const { getByText } = render(<Display closed={false} locked={false} /> );
+    const closed = getByText(/open/i);
+    const locked = getByText(/unlocked/i);
+    expect(closed.classList[1]).toBe('green-led');
+    expect(locked.classList[1]).toBe('green-led');
+  });
+
+  it('Uses red-led class when locked or closed', () => {
+    const { getByText } = render(<Display closed={true} locked={true} /> );
+    const closed = getByText(/closed/i);
+    const locked = getByText(/locked/i);
+    expect(closed.classList[1]).toBe('red-led');
+    expect(locked.classList[1]).toBe('red-led');
   });
 });

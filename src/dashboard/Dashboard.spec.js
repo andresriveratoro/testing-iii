08093bf8e5afc365f30846c79d67f6d8066a2cf4
Andres Rivera <andres.rivera.toro@gmail.com>
2019-02-07 04:29:00 -0500

    Add test for Dashboard component

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,11 @@
-// Test away
+import React from 'react';
+import { render } from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each';
+
+import Dashboard from './Dashboard';
+
+describe('<Dashboard />', () =>{
+  it('Renders without crashing', () => {
+    render(<Dashboard />);
+  });
+});

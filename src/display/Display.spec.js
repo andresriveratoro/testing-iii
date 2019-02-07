2dfd6bc2819cfc86f35163c508a2357404ea8a8f
Andres Rivera <andres.rivera.toro@gmail.com>
2019-02-06 21:27:29 -0500

    Add tests for Display component

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,29 @@
-// Test away!
+import React from 'react';
+import { render } from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each';
+
+import Display from './Display';
+
+
+describe('<Display />', () => {
+  it('Unlocked and open', () => {
+    const { getByText } = render(<Display closed={false} locked={false} /> );
+
+    getByText(/unlocked/i);
+    getByText(/open/i);
+  });
+
+  it('Unlocked and closed', () => {
+    const { getByText } = render(<Display closed={true} locked={false} /> );
+
+    getByText(/unlocked/i);
+    getByText(/closed/i);
+  });
+
+  it('Locked and closed', () => {
+    const { getByText } = render(<Display closed={true} locked={true} /> );
+
+    getByText(/locked/i);
+    getByText(/closed/i);
+  });
+});
